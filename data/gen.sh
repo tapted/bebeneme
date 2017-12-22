856135a967dbe822dc8bf0552d2d7ecdf6aabde7
My Name <my@email>
2017-12-22 16:07:00 +1100

    Remove spaces and hyphens

--- data/gen.sh
+++ data/gen.sh
@@ -1,7 +1,8 @@
 MYSQL="mysql --defaults-file=my.cnf --skip-column-names"
+FILTERS="AND name NOT LIKE '% %' AND name NOT LIKE '%-%'"
 
 for sex in MALE FEMALE ; do
   for start in a b c d e f g h i j k l m n o p q r s t u v w x y z ; do
-    $MYSQL <<< "SELECT name from names WHERE name like '${start}%' AND gender = '${sex}';" | gzip > "${sex}-${start}.gz"
+    $MYSQL <<< "SELECT name from names WHERE name like '${start}%' AND gender = '${sex}' ${FILTERS};" | gzip -9 > "${sex}-${start}.gz"
   done
 done
