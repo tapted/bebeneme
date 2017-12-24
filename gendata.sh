ad556ab0f1cd85b5d96013b107a634fbc17a1d84
My Name <my@email>
2017-12-24 14:55:55 +1100

    Order by ratings total

--- gendata.sh
+++ gendata.sh
@@ -1,8 +1,10 @@
 MYSQL="mysql --defaults-file=my.cnf --skip-column-names"
 FILTERS="AND name NOT LIKE '% %' AND name NOT LIKE '%-%'"
+ORDER="ORDER BY rating_total DESC"
+SELECT="SELECT name from names WHERE name LIKE"
 
 for sex in MALE FEMALE ; do
   for start in a b c d e f g h i j k l m n o p q r s t u v w x y z ; do
-    $MYSQL <<< "SELECT name from names WHERE name like '${start}%' AND gender = '${sex}' ${FILTERS};" | gzip -9 > "${sex}-${start}.gz"
+    $MYSQL <<< "${SELECT} '${start}%' AND gender = '${sex}' ${FILTERS} ${ORDER};" > "app/data/${sex}-${start}.txt"
   done
 done
